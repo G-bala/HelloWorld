04c398ac2742b82483df0dfe630d339d587acd04
ganesh-gbg <ganesh.gbg@gmail.com>
2020-06-03 22:53:59 +0530

    3 st allter

---
+++ helloworld.py
@@ -0,0 +1,41 @@
+from array import array
+
+
+print('Array-234----------')
+lst=array('i',[4,2])
+
+print('list=', lst )
+
+lst[1]=25
+
+print('list=', lst )
+
+print(lst[0])
+
+print('this array has=',len(lst))
+
+print('list----------use [] bracket')
+
+li=['a','b','d']
+
+print('list=', li )
+
+li[2]='r'
+
+print('list=', li )
+
+print(li[2])
+
+print('tulep--------- use () paranthesis')
+
+tu=('a','b','c')
+
+print('list=', tu )
+
+
+print(tu[2])
+
+print('the size is=',len(tu))
+
+
+
